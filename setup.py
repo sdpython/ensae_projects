e914725b4cde4bd75778796395252e742f6b6c1d
dupre <xavier.dupre@ensae.fr>
2016-08-24 01:12:39 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -171,7 +171,7 @@ if is_local():
         additional_local_path=["pyquickhelper",
                                "pyensae", "pyrsslocal", "pymyinstall"],
         copy_add_ext=["enc"], fLOG=logging_function,
-        covtoken=("ad448134-a962-480b-85c6-a899999575d5", "'jenkins' in outfile"))
+        covtoken=("ad448134-a962-480b-85c6-a899999575d5", "'_UT_35_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
                        "upload_docs", "bdist_wininst"} & set(sys.argv)):
