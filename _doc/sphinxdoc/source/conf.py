baadce06a1c2d8751cc19b4caf85ac69cba49744
dupre <xavier.dupre@ensae.fr>
2016-10-29 00:24:38 +0200

    update configuration

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -4,7 +4,8 @@ import os
 import datetime
 import re
 #import sphinxjp.themes.basicstrap
-import sphinx_py3doc_enhanced_theme
+# import sphinx_py3doc_enhanced_theme
+import alabaster
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 sys.path.insert(
@@ -22,11 +23,11 @@ sys.path.insert(
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 set_sphinx_variables(__file__, "ensae_projects", "ENSAE", 2016,
-                     "sphinx_py3doc_enhanced_theme", None,
-                     locals(), add_extensions=None,
+                     "alabaster", # "sphinx_py3doc_enhanced_theme", 
+                     None, locals(), add_extensions=None,
                      extlinks=dict(issue=('https://github.com/sdpython/ensae_projects/issues/%s', 'issue')))
 
-html_theme = 'sphinx_py3doc_enhanced_theme'
+html_theme = 'alabaster', # 'sphinx_py3doc_enhanced_theme'
 html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 
 blog_root = "http://www.xavierdupre.fr/app/ensae_projects/helpsphinx/"
