2af31fb8aa0a78a083172a3d2fa97047c5c93bca
dupre <xavier.dupre@ensae.fr>
2016-10-29 01:51:50 +0200

    configuration

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -28,7 +28,8 @@ set_sphinx_variables(__file__, "ensae_projects", "ENSAE", 2016,
                      extlinks=dict(issue=('https://github.com/sdpython/ensae_projects/issues/%s', 'issue')))
 
 html_theme = 'alabaster', # 'sphinx_py3doc_enhanced_theme'
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+html_theme_path = [alabaster.get_path()]
 
 blog_root = "http://www.xavierdupre.fr/app/ensae_projects/helpsphinx/"
 blog_background = False
