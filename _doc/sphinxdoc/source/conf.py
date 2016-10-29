7288dbd5cbf53e05e7d8eeefe43abc96bb2ba233
dupre <xavier.dupre@ensae.fr>
2016-10-29 14:22:37 +0200

    update notebook and documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -24,10 +24,10 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 set_sphinx_variables(__file__, "ensae_projects", "ENSAE", 2016,
                      "alabaster", # "sphinx_py3doc_enhanced_theme", 
-                     None, locals(), add_extensions=None,
+                     None, locals(), add_extensions=["alabaster"],
                      extlinks=dict(issue=('https://github.com/sdpython/ensae_projects/issues/%s', 'issue')))
 
-html_theme = 'alabaster', # 'sphinx_py3doc_enhanced_theme'
+html_theme = 'alabaster' # 'sphinx_py3doc_enhanced_theme'
 # html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
 html_theme_path = [alabaster.get_path()]
 
