90c38114fa8e8676dec9c992f3d9876d8a133259
dupre <xavier.dupre@ensae.fr>
2016-11-01 00:22:46 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ if is_local():
                                   "pyensae", "pyrsslocal", "pymyinstall"],
         additional_local_path=["pyquickhelper", "jyquickhelper",
                                "pyensae", "pyrsslocal", "pymyinstall"],
-        copy_add_ext=["enc"], fLOG=logging_function,
+        copy_add_ext=["enc"], fLOG=logging_function, layout=['html'],
         covtoken=("ad448134-a962-480b-85c6-a899999575d5", "'_UT_35_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
