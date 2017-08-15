14c750ded721ba4e9ae123b6fd2ba8dae46db09a
xavier dupré <xavier.dupre@gmail.com>
2017-08-15 20:29:05 +0200

    fix notebook unit test

--- src/ensae_projects/automation/notebook_test_helper.py
+++ src/ensae_projects/automation/notebook_test_helper.py
@@ -149,6 +149,9 @@ def execute_notebooks(folder, notebooks, filter,
     kernel_name = None if "travis" in sys.executable else install_python_kernel_for_unittest(
         "ensae_projects")
     addpaths = get_additional_paths()
-
+    if filter:
+        notebooks = [_ for i, _ in enumerate(notebooks) if filter(i, _)]
+    if len(notebooks) == 0:
+        raise ValueError("Empty list of notebooks.")
     return execute_notebook_list(
         folder, notebooks, fLOG=fLOG, valid=_valid_cell, additional_path=addpaths, kernel_name=kernel_name)
