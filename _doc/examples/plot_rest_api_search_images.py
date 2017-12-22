167de327023e16180026242da39ee6f515271df8
xavier dupré <xavier.dupre@gmail.com>
2017-12-22 12:50:44 +0100

    update examples on search engine for images

--- _doc/examples/plot_rest_api_search_images.py
+++ _doc/examples/plot_rest_api_search_images.py
@@ -9,10 +9,13 @@ searches for similar images based on features produced
 by a deep learning model.
 """
 
+import sys
+sys.path.append(r'../../../ensae_projects/src')
+
 ####################
 # Settings.
 host = '127.0.0.1'
-port = 8083
+port = 8085
 
 ########################
 # Creates the search engine and starts a server in a different process.
@@ -36,21 +39,33 @@ def process_server_images(host, port):
     app = search_images_dogcat(url_images=url)
 
     from waitress import serve
+    print("Start")
     serve(app, host=host, port=port)
+
+print("Begin")
 """
 
 ##########################
-# Saves this code into a file and we start it
-# from a different process.
-
+# Saves this code into a file and adds additional paths.
 
 import os
 import ensae_projects
 
+
+def path_module(mod):
+    m = __import__(mod)
+    return os.path.normpath(os.path.abspath(os.path.join(os.path.dirname(m.__file__), '..')))
+
+
+additional_path = ['ensae_projects', 'pyensae', 'pyquickhelper',
+                   'mlinsights', 'lightmlrestapi', 'pandas_streaming']
+paths = [path_module(m) for m in additional_path]
+
+
 header = """
 import sys
-sys.path.append(r'{0}')
-""".format(os.path.join(os.path.dirname(ensae_projects.__file__), '..'))
+{0}
+""".format("\n".join(set("sys.path.append(r'{0}')".format(p) for p in paths)))
 
 code = header + code + \
     "\nprocess_server_images('{0}', {1})\n".format(host, port)
@@ -62,10 +77,17 @@ print("Write file '{0}'.".format(code_file))
 with open(code_file, "w") as f:
     f.write(code)
 
+print('# final code to run')
+print(code)
+
+########################
+# Starts the server.
+
 import sys
 from subprocess import Popen
 if sys.platform.startswith('win'):
-    cmd = '{0} -u "{1}"'.format(sys.executable, code_file)
+    cmd = '{0} -u "{1}"'.format(sys.executable.replace('w.exe',
+                                                       '.exe'), code_file)
     print("Running '{0}'".format(cmd))
     proc = Popen(cmd)
 else:
@@ -105,36 +127,45 @@ import ujson
 b64 = image2base64(imgfile)[1]
 features = ujson.dumps({'X': b64})
 try:
-    r = requests.post('http://127.0.0.1:%d' % port, data=features)
+    r = requests.post('http://127.0.0.1:%d' % port, data=features, timeout=15)
 except Exception as e:
     print("unable to request", 'http://127.0.0.1:%d' % port)
-    raise e
+    print(e)
+    print("We are using dummy data.")
+    r = {'Y': [[[0.0, 1, {'name': 'oneclass\\cat-1192026__480.jpg'}],
+                [0.0185132341, 11, {'name': 'oneclass\\cat-2946028__480.jpg'}],
+                [0.0745846851, 2, {'name': 'oneclass\\cat-1508613__480.jpg'}],
+                [0.0940817104, 28, {
+                    'name': 'oneclass\\shotlanskogo-2934720__480.jpg'}],
+                [0.2563886613, 5, {'name': 'oneclass\\cat-2603300__480.jpg'}]]]}
 
-js = r.json()
-if 'description' in js:
-    # This is an error.
-    print(js['description'])
-    res = None
-else:
-    print(js)
-    res = []
-    for ans in js['Y']:
-        print("Number of neighbors:", len(ans))
-        for n in ans:
-            print("score, id, name", n)
-            res.append((n[0], n[2]['name']))
+if r is not None:
+    js = r if isinstance(r, dict) else r.json()
+    if 'description' in js:
+        # This is an error.
+        print(js['description'])
+        res = None
+    else:
+        print(js)
+        res = []
+        for ans in js['Y']:
+            print("Number of neighbors:", len(ans))
+            for n in ans:
+                print("score, id, name", n)
+                res.append((n[0], n[2]['name']))
 
-#######################
-# Let's display the images.
+    #######################
+    # Let's display the images.
 
-txts = list(map(lambda x: str(x[0]), res))
-imgs = list(map(lambda x: os.path.join('images', x[1]), res))
+    txts = list(map(lambda x: str(x[0]), res))
+    imgs = list(map(lambda x: os.path.join(
+        'temp_scripts', 'images', x[1]), res))
 
-from mlinsights.plotting import plot_gallery_images
-plot_gallery_images(imgs, txts)
+    from mlinsights.plotting import plot_gallery_images
+    plot_gallery_images(imgs, txts)
 
-import matplotlib.pyplot as plt
-# plt.show()
+    # import matplotlib.pyplot as plt
+    # plt.show()
 
 ####################
 # Let's stop the server.
