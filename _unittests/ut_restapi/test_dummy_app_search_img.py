afec701d7c347a673847ddee611156dad2d25721
xavier dupré <xavier.dupre@gmail.com>
2018-04-19 00:51:35 +0200

    update requirements and one unit test

--- _unittests/ut_restapi/test_dummy_app_search_img.py
+++ _unittests/ut_restapi/test_dummy_app_search_img.py
@@ -9,7 +9,7 @@ import unittest
 import ujson
 import falcon
 import falcon.testing as testing
-
+import x86cpu
 
 try:
     import pyquickhelper as skip_
@@ -89,6 +89,7 @@ class TestDummyAppSearchImg(testing.TestCase):
                             'oneclass/wolf-2865653__480.jpg'))
 
     @skipif_travis('tensorflow/python/lib/core/bfloat16.cc:664] Check failed: PyBfloat16_Type.tp_base != nullptr')
+    @unittest.skipIf(not x86cpu.info.supports_avx2, "tensorflow requires instructions AVX2 on CPU")
     def test_dummy_error_img(self):
         fLOG(
             __file__,
