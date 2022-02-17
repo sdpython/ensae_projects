0ea2ed815e043771da0bd8d76b1e2f763568048e
xavier dupré <xavier.dupre@gmail.com>
2022-02-17 23:41:49 +0100

    Update conf.py

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -7,8 +7,7 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
 set_sphinx_variables(__file__, "ensae_projects", "ENSAE", 2021,
-                     "pydata_sphinx_theme", pydata_sphinx_theme.get_html_theme_path(),
-                     locals(),
+                     "pydata_sphinx_theme", ['_static'], locals(),
                      extlinks=dict(issue=('https://github.com/sdpython/ensae_projects/issues/%s', 'issue')))
 
 html_theme_options = {
