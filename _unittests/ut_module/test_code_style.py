7e348b027ed86f38a5211f3f20cf7ea83bcbae20
xavier dupré <xavier.dupre@gmail.com>
2022-07-24 15:53:26 +0200

    update CI

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -24,7 +24,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG, verbose=False, run_cmd_filter=_run_cmd_filter,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'W0212', 'W0107', 'C0415', 'R1725', 'W1514',
                                   'C0209'),
                    skip=["Use % formatting in logging functions and pass the % parameters",
@@ -56,7 +56,7 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*", verbose=False,
                    run_cmd_filter=_run_cmd_filter,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'C0111', 'C0414', 'W0107', 'W0611', 'C0415', 'R1725',
                                   'W1514', 'C0209'),
                    skip=["Module 'ujson' has no 'loads'",
@@ -64,7 +64,6 @@ class TestCodeStyle(ExtTestCase):
                          "Redefining name 'path' from outer scope",
                          "Module 'ujson' has no 'dumps'",
                          "maximum recursion depth exceeded",
-                         "R1720",
                          "Module 'falcon' has no 'HTTP_",
                          ])
 
